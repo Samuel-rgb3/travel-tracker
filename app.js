8e0fd296779fad80776c8d326bb5a71df3223477
SAMUEL RENILDO DE ALMEIDA NOGUEIRA <a95984530@rn.senac.local>
2026-03-25 20:59:14 -0300

    Lógica JS, processamento de Arrays e LocalStorage implementados

--- app.js
+++ app.js
@@ -1,10 +1,77 @@
 const inputDesc = document.querySelector('#desc-despesa');
 const inputValor = document.querySelector('#valor-gringo');
-const inputCotacao = document.querySelector('cotacao');
+const inputCotacao = document.querySelector('#cotacao');
 const btnAdicionar = document.querySelector('#btn-adicionar');
-const listasDespesasDOOM = document.querySelector('#listas-despesas');
-const totalBrlDOOM = document.querySelector('#total-brl');
+const listaDespesasDOM = document.querySelector('#lista-despesas');
+const totalBrlDOM = document.querySelector('#total-brl');
 
 
-let despesas = JSON.parse(localStorage.getItem('viagem-despesas')) || [];
+let despesas = JSON.parse(localStorage.getItem('viagem_despesas')) || [];
 
+const salvarNoLocalStorage = () => {
+    localStorage.setItem('viagem_despesas' , JSON.stringify(despesas));
+    console.log(salvarNoLocalStorage);
+};
+
+const adicionarDespesa = () => {
+    const desc = inputDesc.value.trim();
+    const valorOriginal = parseFloat(inputValor.value);
+    const cotacao = parseFloat(inputCotacao.value);
+
+    console.log(adicionarDespesa);
+
+    if (desc === "" || isNaN(valorOriginal) || isNaN(cotacao)) {
+        alert("Por favor, preencha todos os campos corretamente com válores válidos")
+    
+    return;
+}
+
+
+const valorConvertidoBRL = valorOriginal * cotacao;
+
+const novaDespesa = {
+    id: Date.now(),
+    descricao: desc,
+    valorEstrangeiro: valorOriginal,
+    valorReal: valorConvertidoBRL,
+};
+
+
+despesas.push(novaDespesa);
+salvarNoLocalStorage();
+
+inputDesc.value = "";
+inputValor.value = "";
+inputCotacao.value = "";
+
+atualizarTela();
+};
+
+btnAdicionar.addEventListener('click', adicionarDespesa);
+
+
+const atualizarTela = () => {
+    const htmlDaLista = despesas.map(item => {
+    return `
+        <li>
+            <div>
+                <strong>${item.descricao}</strong> <br>
+                <small>U$ ${item.valorEstrangeiro.toFixed(2)}</small>
+            </div>
+            <span class="valor">R$ ${item.valorReal.toFixed(2)}</span>
+        </li>
+    `;
+});
+
+listaDespesasDOM.innerHTML = htmlDaLista.join('');
+
+let somaTotal = 0;
+
+despesas.forEach(item => {
+    somaTotal += item.valorReal;
+})
+
+totalBrlDOM.textContent = `R$ ${somaTotal.toFixed(2)}`;
+};
+
+atualizarTela();
